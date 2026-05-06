84e17cca26f51c98a6a91f7075a48fdfead858ca
Meeszon <meeszon@gmail.com>
2026-05-06 19:36:47 +0200

    basicSSL removes for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
@@ -12,7 +11,6 @@ export default defineConfig({
     },
   },
   server: {
-    https: true,
     allowedHosts: ['7b89-2a02-a471-1c55-0-434-7237-cbbf-9dbe.ngrok-free.app'],
   },
 })
